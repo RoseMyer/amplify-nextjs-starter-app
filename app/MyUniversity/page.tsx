314333f9ea7b4c4427b90580bb6a6983c0cd7a58
Rose Myer <rosemyer@roses-mbp.mynetworksettings.com>
2024-03-16 08:59:12 -0400

    About BMCC added picture and information at top of the page

--- app/MyUniversity/page.tsx
+++ app/MyUniversity/page.tsx
@@ -4,15 +4,33 @@ import styles from './MyUniversity.module.css'
 export default function AboutBMCC() {
   return (
   <main>
-    <div className={styles.content}>
+    <br/>
+    <div className={styles.topSection}> {/* New class for the top section */} 
+      <div className={styles.imageWrapper}> {/* Wrapper for image */}
+       <Image
+        src="/images/bmcc.jpeg" // Update with the path to your image
+        alt="Borough of Manhattan Community College"
+        width={500} // Set the desired size
+        height={300} // Maintain the aspect ratio of the image
+        className={styles.imageWrapper}
+        />
+      </div>
+      <div className={styles.infoBox}> {/* New class for the info box */}
+        <h4>Borough of Manhattan Community College</h4>
+        <p>199 Chambers Street, New York, NY 10007</p>
+        <p> Founded in 1963, apart of the CUNY schools.</p>
+        {/* Add more information here */}
+      </div>
+    </div>
+
+    <div className={styles.content}> 
     <div className={styles.container}>
 
 
       <br/> <br/>
       <h3> Education and Major </h3> <br/> 
       <p> Borough of Manhattan Community College</p> 
-      <p> Computer Science (A.S) </p> 
-      <p>199 Chambers Street, New York, NY 10007</p><br/>
+      <p> Computer Science (A.S) </p> <br/>
 
       <h3> Current Classes: </h3> <br/>
       <p> CSC 350: Software development</p> 
